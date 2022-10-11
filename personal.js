94df15c857ff2e58d96a0228501d61b19a96c580
Scott Lucchini <scott.lucchini@gmail.com>
2022-10-11 10:40:26 -0400

    Updated research

--- personal.js
+++ personal.js
@@ -3,4 +3,30 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 		$('#nav').toggleClass('open');
 	});
+
+	$('.buttons>a').click(function(){
+		$('.buttons>a').each(function(){
+			$(this).removeClass('active')
+		});
+		$(this).toggleClass('active');
+		showResearch(this)
+	})
+	showResearch($('.buttons>a.active'))
 });
+
+function showResearch(activeButton) {
+	var activeClass = ''
+	$('.buttons>a.active').attr('class').split(' ').forEach(function(item,indx,arr) {
+		if (item != 'active') {
+			activeClass = item
+		}
+	})
+	$('div#body .content').each(function(){
+		$(this).addClass('hidden')
+	})
+	$('div#body .content.'+activeClass).each(function(){
+		$(this).removeClass('hidden')
+	})
+	$('.underliner').css('left',$(activeButton).position().left+14)
+	$('.underliner').css('width',$(activeButton).width()+14)
+}
